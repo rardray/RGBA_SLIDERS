393939aaf0d2ed62d6a8bd5333a8ab463d9cdfe1
Ryan Ardray <rardray@gmail.com>
2018-10-01 23:50:39 -0500

    ling test

--- src/Vcontroll.js
+++ src/Vcontroll.js
@@ -1,81 +1,93 @@
 import React from 'react';
-import './Vcontroll.css'
+import './Vcontroll.css';
 
 class Vcontroll extends React.Component {
-    state = {
-        shiftX: null,
-        mouseDown: false,
+  state = {
+    shiftX: null,
+    mouseDown: false
+  };
+  componentDidMount() {
+    window.addEventListener('mousemove', this.handleMouseMove);
+  }
+  handleMouseDown = e => {
+    e.preventDefault();
+    let thumb = document.querySelector(`.${this.props.thumb}`);
+    //event client x is mouse position  : calculation takes mouse position on click and subtracts it from thumb left position (boundingClientRect left)
+    this.setState(
+      {
+        shiftX: e.clientX - thumb.getBoundingClientRect().left,
+        mouseDown: true
+      },
+      () => console.log(this.state)
+    );
+  };
+  handleMouseMove = e => {
+    if (!this.state.mouseDown) {
+      return;
+    }
+    let slider = document.querySelector(`.${this.props.slider}`);
+    window.addEventListener('mouseup', this.handleMouseUp);
+    // new left takes mouse position subtracts thumb offset then subtracts the sliders left position EX: clientx 60 - shift x 9 = 51 then slider is 40 from left so 51 - 40 = 11... this offsets thumb 11 from slider left
 
+    let newLeft =
+      e.clientX - this.state.shiftX - slider.getBoundingClientRect().left;
+    //if statements set the shift to edges fo slider if mouse x exceeds borders of slider
+    if (newLeft < 0) {
+      newLeft = 0;
     }
-    componentDidMount() {
-        window.addEventListener('mousemove', this.handleMouseMove)
+    let rightEdge =
+      slider.offsetWidth -
+      document.querySelector(`.${this.props.thumb}`).offsetWidth;
+    if (newLeft > rightEdge) {
+      newLeft = rightEdge;
     }
-    handleMouseDown = e => {
-        e.preventDefault()
-        let thumb = document.querySelector(`.${this.props.thumb}`) 
-        console.log(thumb.getBoundingClientRect().left)
-        console.log(e.clientX)
-        //event client x is mouse position  : calculation takes mouse position on click and subtracts it from thumb left position (boundingClientRect left)
-        this.setState({shiftX: e.clientX - thumb.getBoundingClientRect().left, mouseDown: true}, () => console.log(this.state))
+    document.querySelector(`.${this.props.thumb}`).style.left = newLeft + 'px';
+    this.props.setPosition(newLeft);
+  };
+  handleMouseUp = () => {
+    this.setState({ mouseDown: false });
+    window.removeEventListener('mouseup', this.handleMouseUp);
+  };
+  handleClick = e => {
+    let slider = document.querySelector(`.${this.props.slider}`);
+    let position = e.clientX - 7 - slider.getBoundingClientRect().left;
+    if (position < 0) {
+      position = 0;
     }
-    handleMouseMove = e => {
-        if (!this.state.mouseDown) {return;}
-        let slider = document.querySelector(`.${this.props.slider}`);
-        window.addEventListener('mouseup', this.handleMouseUp)
-        // new left takes mouse position subtracts thumb offset then subtracts the sliders left position EX: clientx 60 - shift x 9 = 51 then slider is 40 from left so 51 - 40 = 11... this offsets thumb 11 from slider left
-        console.log(e.clientX)
-        console.log(this.state.shiftX)
-        console.log(slider.getBoundingClientRect().left)
-        let newLeft = e.clientX - this.state.shiftX - slider.getBoundingClientRect().left
-        //if statements set the shift to edges fo slider if mouse x exceeds borders of slider
-        if (newLeft < 0){
-            newLeft = 0;
-            
-        }
-        let rightEdge = slider.offsetWidth - document.querySelector(`.${this.props.thumb}`).offsetWidth;
-        if (newLeft > rightEdge) {
-            newLeft = rightEdge;
-        }
-        document.querySelector(`.${this.props.thumb}`).style.left = newLeft + 'px';
-        this.props.setPosition(newLeft)
-
+    if (position > 255) {
+      position = 255;
     }
-    handleMouseUp = () => {
-        this.setState({mouseDown: false})
-        window.removeEventListener('mouseup', this.handleMouseUp)
-    }
-    handleClick = e => {
-
-        let slider = document.querySelector(`.${this.props.slider}`);
-        let position = e.clientX - 7 - slider.getBoundingClientRect().left;
-        if (position < 0) {
-            position = 0
-        }
-        if (position > 255) {
-            position = 255
-        }
-        document.querySelector(`.${this.props.thumb}`).style.left = position + 'px';
-        this.props.setPosition(position)
-    }
-    handleDrag = () => {return false; }
-    render() {
+    document.querySelector(`.${this.props.thumb}`).style.left = position + 'px';
+    this.props.setPosition(position);
+  };
+  handleDrag = () => {
+    return false;
+  };
+  render() {
     return (
-        <div  style = {{display: 'inline-block'}}
-             >
-           <div id = 'slider' className = {this.props.slider} onClick = {this.handleClick} >
-           <div style = {{
-                            backgroundColor: this.props.color, 
-                            height: '100%', 
-                            width: this.props.value,
-                            borderRadius: 3}}>
-              <div className = {this.props.thumb} 
-                onMouseDown = {this.handleMouseDown} 
-                onDragStart = {this.handleDrag}>
-                </div>
-              </div>
-            </div>
-         </div>
-        )
-    }   
+      <div style={{ display: 'inline-block' }}>
+        <div
+          id="slider"
+          className={this.props.slider}
+          onClick={this.handleClick}
+        >
+          <div
+            style={{
+              backgroundColor: this.props.color,
+              height: '100%',
+              width: this.props.value,
+              borderRadius: 3
+            }}
+          >
+            <div
+              className={this.props.thumb}
+              onMouseDown={this.handleMouseDown}
+              onDragStart={this.handleDrag}
+            />
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 export default Vcontroll;
